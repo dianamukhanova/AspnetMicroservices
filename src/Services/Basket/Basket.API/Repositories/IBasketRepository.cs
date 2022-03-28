e7daf1a4215e96edc69a9e0d5d4fb7dfbd80739b
dianamukhanova <diana.mukhanova@hudl.com>
2022-03-28 18:30:43 +0200

    add basket

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+	public interface IBasketRepository
+	{
+		Task<ShoppingCart> GetBasket(string user);
+		Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+		Task DeleteBasket(string username);
+	}
+}
+
